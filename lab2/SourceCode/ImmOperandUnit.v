8b9abb3c1d8d076f284b2d2a80512a250f0831de
guch8017 <guch8017@gmail.com>
2021-04-14 23:38:12 +0800

    完成立即数生成单元

--- lab2/SourceCode/ImmOperandUnit.v
+++ lab2/SourceCode/ImmOperandUnit.v
@@ -37,8 +37,12 @@ module ImmOperandUnit(
     always@(*)
     begin
         case(Type)
-            `ITYPE: Out<={ {21{In[31]}}, In[30:20] };
-            //......                                        //请完善代码
+            `ITYPE: Out <= { {21{In[31]}}, In[30:20] };
+            `RTYPE: Out <= 32'b0;
+            `STYPE: Out <= { {31{In[31]}}, In[30:25], In[11:7] };
+            `BTYPE: Out <= { {20{In[31]}}, In[7], In[30:25], In[11:8], 1'b0 };
+            `UTYPE: Out <= { In[31:12], 12'b0 };
+            `JTYPE: Out <= { {12{In[31]}}, In[19:12], In[20], In[30:21], 1'b0 };
             default:Out<=32'hxxxxxxxx;
         endcase
     end

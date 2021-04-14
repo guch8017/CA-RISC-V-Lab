ed52eecbf005a2412f008883f978e81ebde9e9d7
guch8017 <guch8017@users.noreply.github.com>
2021-04-14 21:21:53 +0800

    完成ControlUnit

--- lab2/SourceCode/ControlUnit.v
+++ lab2/SourceCode/ControlUnit.v
@@ -31,7 +31,47 @@
 //实验要求  
     //补全模块  
 
-`include "Parameters.v"   
+`include "Parameters.v"
+
+// OpCode
+    `define OP_IMM      7'b0010011  //  SLLI/SRLI/SRAI
+    `define OP_REG      7'b0110011  //  ADD/SUB...
+    `define OP_REGI     7'b0010011  //  ADDI/SLTI...
+    `define OP_LUI      7'b0110111  //  LUI
+    `define OP_AUI      7'b0010111  //  AUIPC
+    `define OP_JALR     7'b1100111  //  JalR
+    `define OP_JAL      7'b1101111  //  Jal             
+    `define OP_BR       7'b1100011  //  条件分支
+    `define OP_LOAD     7'b0000011  //  Load
+    `define OP_STORE    7'b0100011  //  Store
+    `define OP_SYS      0           //  CSR相关指令
+// Funtc 3 ALU
+    `define F3_ADD      3'b000
+    `define F3_SLL      3'b001
+    `define F3_SLT      3'b010
+    `define F3_SLTU     3'b011
+    `define F3_XOR      3'b100
+    `define F3_SRLA     3'b101
+    `define F3_OR       3'b110
+    `define F3_AND      3'b111
+// Funct 3 Branch
+    `define F3_BEQ      3'b000
+    `define F3_BNE      3'b001
+    `define F3_BLT      3'b100
+    `define F3_BGE      3'b101
+    `define F3_BLTU     3'b110
+    `define F3_BGEU     3'b111
+// Funct 3 Load
+    `define F3_LB       3'b000
+    `define F3_LH       3'b001
+    `define F3_LW       3'b010
+    `define F3_LBU      3'b100
+    `define F3_LHU      3'b101
+// Funct 3 Store
+    `define F3_SB       3'b000
+    `define F3_SH       3'b001
+    `define F3_SW       3'b010
+
 module ControlUnit(
     input wire [6:0] Op,
     input wire [2:0] Fn3,
@@ -50,7 +90,213 @@ module ControlUnit(
     output reg [2:0] ImmType        
     ); 
     
-    // 请补全此处代码
+    always @(*) begin
+        case (Op)
+            `OP_IMM: begin
+                RegWriteD <= `LW;
+                ImmType <= `ITYPE;
+                JalD <= 0;
+                JalrD <= 0;
+                MemToRegD <= 0;
+                LoadNpcD <= 0;
+                RegReadD <= 1'b1;
+                BranchTypeD <= `NOBRANCH;
+                AluSrc1D <= 1'b1;
+                AluSrc2D <= 2'd2;
+                MemWriteD <= 4'b0;
+                if (Fn3 == 3'b001 && Fn7 == 7'b0000000) begin
+                    AluContrlD <= `SLL;
+                end else if(Fn3 == 3'b101 && Fn7 == 7'b0000000)begin
+                    AluContrlD <= `SRL;
+                end else begin
+                    AluContrlD <= `SRA;
+                end
+            end
+
+            `OP_REG: begin
+                // 通用赋值
+                RegWriteD <= `LW;
+                ImmType <= `ITYPE;
+                JalD <= 0;
+                JalrD <= 0;
+                MemToRegD <= 0;
+                LoadNpcD <= 0;
+                RegReadD <= 1'b1;
+                BranchTypeD <= `NOBRANCH;
+                AluSrc1D <= 1'b1;
+                AluSrc2D <= 2'b0;
+                MemWriteD <= 4'b0;
+                // Funct 特异
+                case (Fn3)
+                    `F3_ADD: AluContrlD <= (Fn7 == 7'b0100000) ? `SUB : `ADD;
+                    `F3_SLL: AluContrlD <= `SLL;
+                    `F3_SLT: AluContrlD <= `SLT;
+                    `F3_SLTU: AluContrlD <= `SLTU;
+                    `F3_XOR: AluContrlD <= `XOR;
+                    `F3_SRLA: AluContrlD <= (Fn7 == 7'b0100000) ? `SRA : `SRL;
+                    `F3_OR:  AluContrlD <= `OR;
+                    `F3_AND: AluContrlD <= `AND;
+                    default: AluContrlD <= 0;
+                endcase
+            end
+
+            `OP_REGI: begin
+                // 通用赋值
+                RegWriteD <= `LW;
+                ImmType <= `ITYPE;
+                JalD <= 0;
+                JalrD <= 0;
+                MemToRegD <= 0;
+                LoadNpcD <= 0;
+                RegReadD <= 1'b1;
+                BranchTypeD <= `NOBRANCH;
+                AluSrc1D <= 1'b1;
+                AluSrc2D <= 2'd2;
+                MemWriteD <= 4'b0;
+                // Funct 特异
+                case (Fn3)
+                    `F3_ADD: AluContrlD <= `ADD;
+                    `F3_SLT: AluContrlD <= `SLT;
+                    `F3_SLTU: AluContrlD <= `SLTU;
+                    `F3_XOR: AluContrlD <= `XOR;
+                    `F3_OR: AluContrlD <= `OR;
+                    `F3_AND: AluContrlD <= `AND; 
+                    default: AluContrlD <= 0;
+                endcase
+            end
+
+            `OP_LUI: begin
+                RegWriteD <= `LW;
+                ImmType <= `UTYPE;
+                JalD <= 0;
+                JalrD <= 0;
+                MemToRegD <= 0;
+                LoadNpcD <= 0;
+                RegReadD <= 0;
+                BranchTypeD <= `NOBRANCH;
+                AluSrc1D <= 0;  // 无所谓
+                AluSrc2D <= 2'd2;
+                AluContrlD <= `LUI;
+                MemWriteD <= 4'b0;
+            end
+
+            `OP_AUI: begin
+                RegWriteD <= `LW;
+                ImmType <= `UTYPE;
+                JalD <= 0;
+                JalrD <= 0;
+                MemToRegD <= 0;
+                LoadNpcD <= 0;
+                RegReadD <= 0;
+                BranchTypeD <= `NOBRANCH;
+                AluSrc1D <= 0;      // PcE
+                AluSrc2D <= 2'd2;   // ImmE
+                AluContrlD <= `ADD;
+                MemWriteD <= 4'b0;
+            end
+
+            `OP_JALR: begin
+                RegWriteD <= `LW;
+                ImmType <= `UTYPE;
+                JalD <= 0;
+                JalrD <= 1'b1;      // Jalr
+                MemToRegD <= 0;     // FromALU
+                LoadNpcD <= 1'b1;   // PC
+                RegReadD <= 1'b1;   // Read
+                BranchTypeD <= `NOBRANCH;
+                AluSrc1D <= 0;      // PcE
+                AluSrc2D <= 2'd2;   // ImmE
+                AluContrlD <= `ADD;
+                MemWriteD <= 4'b0;
+            end
+
+            `OP_JAL: begin
+                RegWriteD <= `LW;
+                ImmType <= `JTYPE;
+                JalD <= 1'b1;       // Jal
+                JalrD <= 0;
+                MemToRegD <= 0;     // FromALU
+                LoadNpcD <= 1'b1;   // PC
+                RegReadD <= 0;
+                BranchTypeD <= `NOBRANCH;
+                AluSrc1D <= 0;      // 随意
+                AluSrc2D <= 2'd2;   // 随意
+                AluContrlD <= `ADD; // 随意
+                MemWriteD <= 4'b0;
+            end
+
+            `OP_BR: begin
+                RegWriteD <= `NOREGWRITE;
+                ImmType <= `BTYPE;
+                JalD <= 0;
+                JalrD <= 0;
+                MemToRegD <= 0;     // 随意
+                LoadNpcD <= 1'b1;   // 随意
+                RegReadD <= 1'b1;   // Read          
+                AluSrc1D <= 0;      // 随意
+                AluSrc2D <= 2'd2;   // 随意
+                AluContrlD <= `ADD; // 随意
+                MemWriteD <= 4'b0;
+
+                case (Fn3)
+                    `F3_BEQ: BranchTypeD <= `BEQ;
+                    `F3_BNE: BranchTypeD <= `BNE;
+                    `F3_BLT: BranchTypeD <= `BLT;
+                    `F3_BGE: BranchTypeD <= `BGE;
+                    `F3_BLTU: BranchTypeD <= `BLTU;
+                    `F3_BGEU: BranchTypeD <= `BGEU;
+                    default: BranchTypeD <= `NOBRANCH;
+                endcase
+            end
+
+            `OP_LOAD: begin
+                ImmType <= `ITYPE;
+                JalD <= 0;
+                JalrD <= 0;
+                MemToRegD <= 1'b1;  // FromMem
+                LoadNpcD <= 0;
+                RegReadD <= 1'b1;   // Read
+                BranchTypeD <= `NOBRANCH;
+                AluSrc1D <= 1'b1;   // From Reg
+                AluSrc2D <= 2'd2;   // From Imme
+                AluContrlD <= `ADD; // Reg + Imme
+                MemWriteD <= 4'b0;
+
+                case (Fn3)
+                    `F3_LB: RegWriteD <= `LB;
+                    `F3_LH: RegWriteD <= `LH;
+                    `F3_LW: RegWriteD <= `LW;
+                    `F3_LBU: RegWriteD <= `LBU;
+                    `F3_LHU: RegWriteD <= `LHU;
+                    default: RegWriteD <= `NOREGWRITE;
+                endcase
+            end
+
+            `OP_STORE: begin
+                ImmType <= `STYPE;
+                JalD <= 0;
+                JalrD <= 0;
+                MemToRegD <= 0;     // 随意
+                LoadNpcD <= 0;
+                RegReadD <= 1'b1;   // Read
+                BranchTypeD <= `NOBRANCH;
+                AluSrc1D <= 1'b1;   // From Reg
+                AluSrc2D <= 2'd2;   // From Imme
+                AluContrlD <= `ADD; // Reg + Imme
+                RegWriteD <= `NOREGWRITE;
+                
+                case (Fn3)
+                    `F3_SB: MemWriteD <= 4'b0001;
+                    `F3_SH: MemWriteD <= 4'b0011;
+                    `F3_SW: MemWriteD <= 4'b1111;
+                    default: MemWriteD <= 4'b0;
+                endcase
+                
+            end
+
+            default: 
+        endcase
+    end
 
 endmodule
 
--- lab2/SourceCode/Parameters.v
+++ lab2/SourceCode/Parameters.v
@@ -17,17 +17,17 @@
 `ifndef CONST_VALUES
 `define CONST_VALUES
 //ALUContrl[3:0]
-    `define SLL  4'd0
-    `define SRL  4'd1
-    `define SRA  4'd2
-    `define ADD  4'd3
-    `define SUB  4'd4
-    `define XOR  4'd5
-    `define OR  4'd6
-    `define AND  4'd7
-    `define SLT  4'd8
-    `define SLTU  4'd9
-    `define LUI  4'd10
+    `define SLL  4'd0   // 4'b0000
+    `define SRL  4'd1   // 4'b0001
+    `define SRA  4'd2   // 4'b0010
+    `define ADD  4'd3   // 4'b0011
+    `define SUB  4'd4   // 4'b0100
+    `define XOR  4'd5   // 4'b0101
+    `define OR  4'd6    // 4'b0110
+    `define AND  4'd7   // 4'b0111
+    `define SLT  4'd8   // 4'b1000
+    `define SLTU  4'd9  // 4'b1001
+    `define LUI  4'd10  // 4'b1010
 //BranchType[2:0]
     `define NOBRANCH  3'd0
     `define BEQ  3'd1
@@ -50,4 +50,6 @@
     `define LW  3'd3			//	write 32bit to Register
     `define LBU  3'd4			//	load 8bit from Mem then unsigned extended to 32bit
     `define LHU  3'd5			//	load 16bit from Mem then unsigned extended to 32bit
+
+
 `endif
